5b098ce4b84dc499bcf88442c778976074925dc5
NirinaEddiano <anoeddi84@gmail.com>
2025-09-23 19:12:31 +0000

    Fix: Chemin d'importation dans la page de profil

--- src/app/admin/profile/page.jsx
+++ src/app/admin/profile/page.jsx
@@ -1,6 +1,6 @@
 import { PrismaClient } from '@prisma/client';
 import { getServerSession } from 'next-auth/next';
-import { authOptions } from "../../api/auth/[...nextauth]/route";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import ProfileForm from './ProfileForm';
 
 const prisma = new PrismaClient();
