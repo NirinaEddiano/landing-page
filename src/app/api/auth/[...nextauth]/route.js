7cf8eee66026f615b4cc3a7cf0ba23fc1421a975
NirinaEddiano <anoeddi84@gmail.com>
2025-09-24 18:43:10 +0000

    Préparation pour le déploiement

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -4,13 +4,12 @@ import { authOptions } from '../../../../lib/auth';
 
 const handler = NextAuth(authOptions);
 
-// Exporter les méthodes GET et POST séparément avec une gestion d'erreurs
 export async function GET(req, res) {
   try {
     return await handler(req, res);
   } catch (error) {
     console.error("NextAuth GET Error:", error);
-    // En cas d'erreur, retourner une réponse JSON et un statut 500
+
     return new Response(
       JSON.stringify({ error: "Internal Server Error" }),
       { status: 500, headers: { "Content-Type": "application/json" } }
@@ -23,7 +22,7 @@ export async function POST(req, res) {
     return await handler(req, res);
   } catch (error) {
     console.error("NextAuth POST Error:", error);
-    // En cas d'erreur, retourner une réponse JSON et un statut 500
+
     return new Response(
       JSON.stringify({ error: "Internal Server Error" }),
       { status: 500, headers: { "Content-Type": "application/json" } }
